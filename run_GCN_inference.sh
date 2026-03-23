cb4332998c5526cca51ce66d3e37fb872666dad8
Gihyeon Kim <39871769+gyyeon@users.noreply.github.com>
2026-03-23 18:01:04 +0900

    Update run_GCN_inference.sh

--- run_GCN_inference.sh
+++ run_GCN_inference.sh
@@ -6,9 +6,9 @@ echo "Starting GCN_inference.py"
 export CUDA_VISIBLE_DEVICES=0
 
 python GCN_inference.py \
-  -fd ./result_latent_CV/Contrastive_Supervised_Adversarial_ae_CV/[All_latent]_244_d750_adv0.05_con0.2/latent_external_val.csv \
-  -ad ./result/latent_snf_K15/244_d750/SNF_fused_val.csv \
-  -sl ./data/validation_sample.csv \
+  -fd ./result_latent_CV/Contrastive_Supervised_Adversarial_ae_CV/[All_latent]_244_d750_adv0.05_con0.2/latent_inference.csv \
+  -ad ./result/latent_snf_K15/244_d750/SNF_fused_inference.csv \
+  -sl ./data/inference_sample.csv \
   -ld ./data/sample_classes_merged.csv \
   --binary_model_path ./model_pth/GCN/CDTOO_gcn/244_d750_adv0.05_con0.2_thsd0.99_Drop0.1/best_binary_model.pkl \
   --too_model_path ./model_pth/GCN/CDTOO_gcn/244_d750_adv0.05_con0.2_thsd0.99_Drop0.1/best_too_model.pkl \
