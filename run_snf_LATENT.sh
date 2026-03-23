cacb4b9584fa69f2fa6111c23940d82deb6bd818
Yumi Lee <151449172+hrb0@users.noreply.github.com>
2026-03-23 17:25:32 +0900

    Update run_snf_LATENT.sh

--- run_snf_LATENT.sh
+++ run_snf_LATENT.sh
@@ -11,6 +11,7 @@ python SNF_latent_total.py \
   -mp ./result_latent_CV/Contrastive_Supervised_Adversarial_ae_CV/[All_latent]_244_d750_adv0.05_con0.2 \
   -train_p latent_train_met.csv latent_train_fsr.csv latent_train_cnv.csv \
   -test_p latent_test_met.csv latent_test_fsr.csv latent_test_cnv.csv \
+  -val_p latent_external_val_met.csv latent_external_val_fsr.csv latent_external_val_cnv.csv \
   -tl ./data/test_sample.csv \
   -k 15 \
   -m sqeuclidean  \
